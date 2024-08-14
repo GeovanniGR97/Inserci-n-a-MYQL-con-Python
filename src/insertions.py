7ae1b5e3da8f24fae7b0fd9db760f5968f3d12f0
JoseGeovanniRDZ <geovanni97rodriguez@gmail.com>
2024-08-14 17:13:38 -0600

    insertions of data

--- src/insertions.py
+++ src/insertions.py
@@ -1,7 +1,6 @@
 from  mysql.connector import Error
 import mysql.connector
 from random_data import data
-print(data)
 try:
     connection = mysql.connector.connect(
         host='localhost',
@@ -11,7 +10,17 @@ try:
         db='mysql_python_conection'
     )
     if connection.is_connected():
-        print("Ok!")
+        cursor=connection.cursor()
+        cursor.executemany("""INSERT INTO persons (id, name, company, job, email, phone, mac_address)
+                           VALUES(%s, %s, %s, %s, %s, %s, %s)""", data)
+        #print(cursor.rowcount)
+        #print(len(data))
+        if (len(data) == cursor.rowcount):
+            connection.commit()
+            print("{} row inserted.".format(len(data)))
+        else:
+            connection.rollback()
+
 except Error as ex:
     print("Error during connection: {}".format(ex))
 finally:
